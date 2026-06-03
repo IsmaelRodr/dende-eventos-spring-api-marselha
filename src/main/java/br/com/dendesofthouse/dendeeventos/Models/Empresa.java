8d88f4d8af51bc1c47b26479584a014b2035cefa
Ismael Rodrigues <162070086+ismaelrodr@users.noreply.github.com>
2026-06-03 17:11:34 -0300

    versionando models

---
+++ src/main/java/br/com/dendesofthouse/dendeeventos/Models/Empresa.java
@@ -0,0 +1,48 @@
+package br.com.dendesofthouse.dendeeventos.Models;
+
+import br.com.dendesofthouse.dendeeventos.Models.Organizador;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "empresa")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+
+public class Empresa {
+
+    @Id
+    @NotBlank(message = "CNPJ é obrigatório")
+    @Column(name = "cnpj", length = 18, nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private String cnpj;
+
+
+    @NotBlank(message = "Razão social é obrigatória")
+    @Column(name = "razao_social", length = 255, nullable = false)
+    @ToString.Include
+    private String razaoSocial;
+
+    @NotBlank(message = "Nome Fantasia é obrigatório")
+    @Column(name = "nome_fantasia", length = 255, nullable = false)
+    @ToString.Include
+    private String nomeFantasia;
+
+    // Relacionamento ManyToOne com Organizador (assumindo que a entidade Organizador existe)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "organizador_id",
+            nullable = false,
+            unique = true,
+            foreignKey = @ForeignKey(name = "fk_empresa_organizador")
+    )
+    @ToString.Exclude // evita loop no toString
+    private Organizador organizador;
+}
